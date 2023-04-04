edf134bc277e7e6a9366ee24052f277491f97e6a
Stefanie Brandl <stefanie.brandl@sap.com>
2023-04-04 19:15:38 +0200

    Anzeige Weine

--- app/src/main/java/com/example/newkork/NewActivity.java
+++ app/src/main/java/com/example/newkork/NewActivity.java
@@ -1,26 +1,39 @@
 package com.example.newkork;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class NewActivity extends AppCompatActivity{
+
+    Button suchewein_button;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
-        RecyclerView recyclerView;
-        FloatingActionButton add_button;
-        ImageView empty_imageview;
-        TextView no_data;
 
-        Database myDB;
-        ArrayList<String> wineID, wineName, wineType;
+
+
+
+
+        suchewein_button = findViewById(R.id.suchewein_button);
+
+        suchewein_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(NewActivity.this, NewActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
     }
+
+
+
+
 }
