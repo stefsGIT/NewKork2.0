c4359842240646b5255e70342d621f6d96423bde
RobinFrick3 <68026178+RobinFrick3@users.noreply.github.com>
2023-04-04 14:11:51 +0200

    Änderungen

--- app/src/main/java/com/example/newkork/MainActivity.java
+++ app/src/main/java/com/example/newkork/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.newkork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
         Database Weine = new Database(MainActivity.this);
         CreateTable Loader = new CreateTable();
-        Weine = Loader.loadData(Weine);
 
-        rotwein_button = findViewById(R.id.Rotwein);
-        weiswein_button = findViewById(R.id.Weiswein);
+        Cursor cursorW = Weine.readData();
+        if (cursorW.getCount()==0){
+            Weine = Loader.loadData(Weine);
+        }
+        Cursor cursorR = Weine.readData();
+        if (cursorR.getCount()==0){
+            Weine = Loader.loadData(Weine);
+        }
+
+        rotwein_button = findViewById(R.id.Vegi);
+        weiswein_button = findViewById(R.id.Fleisch);
         suche_button = findViewById(R.id.Suchen);
 
         rotwein_button.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/newkork/createRecipeTable.java
@@ -0,0 +1,10 @@
+package com.example.newkork;
+
+public class createRecipeTable {
+    public DatabaseRecept loadData(DatabaseRecept db){
+
+        db.add("Brathähnchen mit Kräuterbutter und Zitrone", "","","","", "");
+
+        return (db);
+    }
+}
